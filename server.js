8942f0043003d244f0f6f63ef50a9177b4451634
guccijenkins <61069083+guccijenkins@users.noreply.github.com>
2025-03-18 10:51:37 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,68 @@
+// This is your test secret API key.
+const express = require('express');
+const cors = require('cors');
+const stripe = require('stripe')('sk_test_51Ql16dAgLF2qD5NrppdWyqILJctHV7ejPJOivJNffp89IrVdc1D6k4LZVYpdoUh5oAAQMfs79Y66Ik4pJrtp9xCU00ZGkdmCYQ');
+
+const app = express();
+
+const corsOptions = {
+  origin: ['http://localhost:4242', 'null', 'https://guccijenkins.github.io'],
+  methods: ['GET', 'POST'],
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json()); // For parsing JSON request bodies
+app.use(express.urlencoded({ extended: true })); // For parsing form data
+app.use(express.static('public'));
+
+const YOUR_DOMAIN = 'http://localhost:4242';
+
+app.post('/create-checkout-session', async (req, res) => {
+
+
+    // Extract priceIds from the request body
+    const priceIds = req.body.priceIds || [];
+    
+    // Check if we have any price IDs
+    if (!priceIds.length) {
+      return res.status(400).json({ error: 'No price IDs provided' });
+    }
+
+  const taxRate = await stripe.taxRates.create({
+    display_name: 'US Sales Tax',
+    inclusive: false,
+    percentage: 7.25,
+    country: 'US',
+    
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    billing_address_collection: 'auto',
+    shipping_address_collection: {
+      allowed_countries: ['US', 'CA'],
+    },
+
+    line_items: priceIds.map(priceId => ({
+      price: priceId,
+      quantity: 1,
+      tax_rates: [taxRate.id],
+      adjustable_quantity: {
+        enabled: true,
+        minimum: 1,
+        maximum: 99,
+      },
+    })),
+
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}/success.html`,
+    cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+    
+  });
+
+  res.json({ url: session.url });
+});
+
+app.listen(4242, () => console.log('Running on port 4242'));
