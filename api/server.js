9926c15ed47f0b6c840d8971c54c0cdd620fae72
guccijenkins <61069083+guccijenkins@users.noreply.github.com>
2025-03-19 10:22:32 -0600

    Update server.js

--- api/server.js
+++ api/server.js
@@ -57,8 +57,8 @@ app.post('/create-checkout-session', async (req, res) => {
     })),
 
     mode: 'payment',
-    success_url: `${YOUR_DOMAIN}/success.html`,
-    cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+    success_url: `${YOUR_DOMAIN}/public/success.html`,
+    cancel_url: `${YOUR_DOMAIN}/public/cancel.html`,
     
   });
 
